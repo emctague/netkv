a91fd3488154308aae8caf37388c59f6e34af3c3
Ethan McTague <ethan@tague.me>
2019-10-14 19:14:50 -0400

    Fix issue with crash termination.

--- exit_handler.c
+++ exit_handler.c
@@ -3,11 +3,13 @@
 
 #include "main.h"
 #include <signal.h>
+#include <stdlib.h>
 
 db_state *exit_handler_db_state = NULL;
 
 void on_exit(int _) {
     db_state_deinit(exit_handler_db_state);
+    exit(0);
 }
 
 void bind_exit_handler(db_state *state) {
